5e56c4585d4d0e3cb548663436cfcb40e74364ea
dudamollica <dudarenno@yahoo.com.br>
2023-01-19 15:33:22 -0300

    Cadastrando usuario

--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,20 +1,71 @@
 import Logo from "../Assets/MyWallet.png";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
-import { Container, FormStyle, LinkStyle} from "../Constants/StyledsComponents";
+import {
+  Container,
+  FormStyle,
+  LinkStyle,
+} from "../Constants/StyledsComponents";
+import { useState } from "react";
+import axios from "axios";
 
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passConfirm, setPassConfirm] = useState("");
+  const navigate = useNavigate();
+
+  function submitRegister(e) {
+    e.preventDefault();
+    const url = "http://localhost:5000/cadastro";
+    const body = { name, email, password };
+
+    if (password === passConfirm) {
+      const promisse = axios.post(url, body);
+      promisse.then(() => navigate("/"));
+      promisse.catch((err) => console.log(err));
+    } else {
+      alert("As senhas são diferentes");
+    }
+  }
+
   return (
     <Container>
       <LogoStyle src={Logo} />
 
-      <FormStyle>
-        <input type="text" placeholder="Nome"></input>
-        <input type="email" placeholder="E-mail"></input>
-        <input type="password" placeholder="Senha"></input>
-        <input type="password" placeholder="Confirme a senha"></input>
+      <FormStyle onSubmit={submitRegister}>
+        <input
+          type="text"
+          placeholder="Nome"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="email"
+          placeholder="E-mail"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Senha"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          required
+          value={passConfirm}
+          onChange={(e) => setPassConfirm(e.target.value)}
+        ></input>
         <button type="submit">Cadastrar</button>
       </FormStyle>
+
       <LinkStyle>
         <Link to="/">Já tem uma conta? Entre agora!</Link>
       </LinkStyle>
